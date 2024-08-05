aa5e01eb386775a5422b8aa9096ab9a2eb4ec2ed
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-08-06 04:00:32 +0530

    committing main.py

--- Project/main.py
+++ Project/main.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin,login_user,logout_user,login_manager,LoginManager,login_required,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 from sqlalchemy import text
+from flask_mail import Mail
+import json
 
 #my database connection
 local_server = True
@@ -17,12 +19,24 @@ login_manager.login_view='login'
 def load_user(user_id):
       return User.query.get(int(user_id))
 
-
-
-
-
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/hms'
 db=SQLAlchemy(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # here we will be creating db models i.e tables --------------->>>>
 class Test(db.Model):
@@ -43,12 +57,24 @@ class Patients(db.Model):
      gender=db.Column(db.String(50))
      slot=db.Column(db.String(50))
      disease=db.Column(db.String(50))
-     time=db.Column(db.String(50),nullable=False)
-     date=db.Column(db.String(50),nullable=False)
+     time = db.Column(db.String(50), nullable=False)
+     date = db.Column(db.String(50), nullable=False)
      dept=db.Column(db.String(50))
      number=db.Column(db.String(50))
 
+class Doctors(db.Model):
+    did = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(50),unique=True)
+    doctorname = db.Column(db.String(50))
+    dept = db.Column(db.String(50))
 
+class Trigr(db.Model):
+    tid = db.Column(db.Integer,primary_key=True)
+    pid = db.Column(db.Integer)
+    email = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    action = db.Column(db.String(50))
+    timestamp = db.Column(db.String(50))
 
 
 
@@ -63,13 +89,27 @@ class Patients(db.Model):
 def index():
         return render_template('index.html')
 
-@app.route('/doctors')
+@app.route('/doctors', methods=['POST', 'GET'])
+@login_required
 def doctors():
-        return render_template('doctors.html')
+    if request.method == "POST":
+        email = request.form.get('email')
+        doctorname = request.form.get('doctorname')
+        dept = request.form.get('dept')
+        new_doctor = Doctors(
+            email=email,
+            doctorname=doctorname,
+            dept=dept,
+        )
+        db.session.add(new_doctor)
+        db.session.commit()
+        flash("Registration confirmed", "info")
+    return render_template('doctors.html')
 
 @app.route('/patients', methods=['POST', 'GET'])
 @login_required
 def patients():
+    doct = Doctors.query.all()  # ORM way to retrieve all doctors
     if request.method == "POST":
         email = request.form.get('email')
         name = request.form.get('name')
@@ -92,12 +132,11 @@ def patients():
             dept=dept,
             number=number
         )
-
         db.session.add(new_patient)
         db.session.commit()
 
         flash("Booking confirmed", "info")
-    return render_template('patients.html')
+    return render_template('patients.html',doct=doct)
 
 @app.route('/bookings')
 @login_required
@@ -108,6 +147,54 @@ def bookings():
         result = conn.execute(query, {'em': em}).fetchall()
     return render_template('bookings.html', query=result)
 
+@app.route("/update/<string:pid>", methods=['POST', 'GET'])
+@login_required
+def update(pid):
+    if request.method == "POST":
+        email = request.form.get('email')
+        name = request.form.get('name')
+        gender = request.form.get('gender')
+        slot = request.form.get('slot')
+        disease = request.form.get('disease')
+        time = request.form.get('time')
+        date = request.form.get('date')
+        dept = request.form.get('dept')
+        number = request.form.get('number')
+        sql = text("""
+            UPDATE patients
+            SET email = :email, name = :name, gender = :gender, slot = :slot, disease = :disease, time = :time, date = :date, dept = :dept, number = :number
+            WHERE pid = :pid
+        """)
+        db.session.execute(sql, {
+            'email': email,
+            'name': name,
+            'gender': gender,
+            'slot': slot,
+            'disease': disease,
+            'time': time,
+            'date': date,
+            'dept': dept,
+            'number': number,
+            'pid': pid
+        })
+        db.session.commit()
+        flash("Slot updated")
+        return redirect('/bookings')
+    patient = Patients.query.filter_by(pid=pid).first()
+    return render_template("update.html", posts=patient)
+
+@app.route("/delete/<string:pid>", methods=['POST', 'GET'])
+@login_required
+def delete(pid):
+    patient = Patients.query.filter_by(pid=pid).first()
+    if patient:
+        db.session.delete(patient)
+        db.session.commit()
+        flash("Deletion done", "danger")
+    else:
+        flash("Patient not found", "warning")
+    return redirect('/bookings')
+
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == "POST":
@@ -115,7 +202,6 @@ def signup():
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
-        
         if user:
             flash("Email already exists", "warning")
             return render_template("signup.html", error="Email already exists")
@@ -132,7 +218,6 @@ def signup():
             db.session.rollback()
             flash("An error occurred during signup", "danger")
             return render_template("signup.html", error="An error occurred during signup")
-
     # GET request handling
     return render_template("signup.html")
 
@@ -143,7 +228,6 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
-
         if user and check_password_hash(user.password, password):
             login_user(user)  # This should log in the user
             flash("login done","primary") 
@@ -169,5 +253,31 @@ def test():
     except:
         return 'my db is not conn'
 
+@app.route('/search', methods=['POST', 'GET'])
+@login_required
+def search():
+    if request.method == 'POST':
+        query = request.form.get('search')
+        
+        # Search by department
+        dept = Doctors.query.filter_by(dept=query).first()
+        
+        # Search by doctor name
+        name = Doctors.query.filter_by(doctorname=query).first()
+        
+        if dept:
+            flash("Department is Available", "info")
+        elif name:
+            flash("Doctor is Available", "info")
+        else:
+            flash("No matching department or doctor found", "danger")
+
+    return render_template('index.html')
+
+@app.route('/details')
+@login_required
+def details():
+    posts = Trigr.query.all()
+    return render_template('trigers.html',posts=posts)
 
 app.run(debug=True)
